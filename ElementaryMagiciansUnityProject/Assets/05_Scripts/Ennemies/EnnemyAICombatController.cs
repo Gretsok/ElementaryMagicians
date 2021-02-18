6adc6b752b672ff2b81c7ec9ef911ae32d69fc00
Gretsok <47755843+Gretsok@users.noreply.github.com>
2021-02-18 21:06:08 +0100

    PREPARED FOR CODING SPELLS

---
+++ ElementaryMagiciansUnityProject/Assets/05_Scripts/Ennemies/EnnemyAICombatController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ElementaryMagicians.Ennemy
+{
+    public class EnnemyAICombatController : Combat.CombatController
+    {
+        protected override void Die()
+        {
+            base.Die();
+            MOtter.MOtterApplication.GetInstance().GAMEMANAGER.GetCurrentMainStateMachine<Dunjeon.DunjeonRoomGameMode>().Ennemies.Remove(GetComponent<EnnemyAI>());
+            Destroy(gameObject);
+        }
+    }
+}
