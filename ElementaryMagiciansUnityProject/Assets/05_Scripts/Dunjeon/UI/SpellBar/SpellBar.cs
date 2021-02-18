0c6a62f5de3d58b5e7407f51d07cd95c264d10fd
Chloii <66557211+Chloii@users.noreply.github.com>
2021-02-19 00:47:42 +0100

    UI changement

---
+++ ElementaryMagiciansUnityProject/Assets/05_Scripts/Dunjeon/UI/SpellBar/SpellBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ElementaryMagicians.Dunjeon
+{
+
+    public class SpellBar : MonoBehaviour
+    {
+    
+    }
+
+}
