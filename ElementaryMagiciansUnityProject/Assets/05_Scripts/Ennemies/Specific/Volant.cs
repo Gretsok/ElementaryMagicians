7c3dbae8e4e6ff511f158f643e9d51c2379c10f2
Gretsok <47755843+Gretsok@users.noreply.github.com>
2021-02-19 18:41:09 +0100

    Volant

--- ElementaryMagiciansUnityProject/Assets/05_Scripts/Ennemies/Specific/Volant.cs
+++ ElementaryMagiciansUnityProject/Assets/05_Scripts/Ennemies/Specific/Volant.cs
@@ -6,6 +6,14 @@ namespace ElementaryMagicians.Ennemy
 {
     public class Volant : EnnemyAI
     {
-        
+        [SerializeField]
+        private RangeMonsterDamageDealer m_volantProjectilePrefab = null;
+        internal override void Attack()
+        {
+            base.Attack();
+            RangeMonsterDamageDealer projectile = Instantiate(m_volantProjectilePrefab);
+            projectile.Init(transform.position + transform.forward, transform.position + transform.forward * 1.5f, CombatController);
+            projectile.Cast();
+        }
     }
 }
