b6fae0203434483d5b55157ccb2b1a27711d299d
Gretsok <47755843+Gretsok@users.noreply.github.com>
2021-02-19 14:07:30 +0100

    PlantMage

--- ElementaryMagiciansUnityProject/Assets/05_Scripts/Player/MagicianController/SpecificMagicians/PlantMagicianController.cs
+++ ElementaryMagiciansUnityProject/Assets/05_Scripts/Player/MagicianController/SpecificMagicians/PlantMagicianController.cs
@@ -6,9 +6,23 @@ namespace ElementaryMagicians.Player
 {
     public class PlantMagicianController : MagicianController
     {
+        [SerializeField]
+        private PlantHealZone m_plantHealZonePrefab = null;
+
         internal override void PrimaryAttack()
         {
-            base.PrimaryAttack();
+            try
+            {
+                base.PrimaryAttack();
+                Instantiate(m_plantHealZonePrefab, transform.position, Quaternion.identity);
+            }
+            catch (System.Exception e)
+            {
+                if (e is OnCooldownException)
+                {
+                    Debug.LogWarning("Spell is on cooldown");
+                }
+            }
 
         }
 
---
+++ ElementaryMagiciansUnityProject/Assets/05_Scripts/Player/MagicianController/SpecificMagicians/Spells/Plant/PlantHealZone.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ElementaryMagicians.Player
+{
+    public class PlantHealZone : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject m_plantsContainer = null;
+        [SerializeField]
+        private SphereCollider m_collider = null;
+
+        [SerializeField]
+        private float m_lifeSpan = 8f;
+        [SerializeField]
+        private int m_amountToHeal = 40;
+        [SerializeField]
+        private float m_healCooldown = 0.7f;
+        private float m_timeOfLastHealWave = float.MinValue;
+
+        [Space(5f)]
+
+        [SerializeField]
+        private float m_startingLocalHeight = -3f;
+        [SerializeField]
+        private float m_finishingLocalHeight = 0f;
+        [SerializeField]
+        private float m_comingOutOfGroundDuration = 1f;
+        [SerializeField]
+        private float m_goingIntoGroundDuration = 1f;
+
+        private List<Combat.CombatCollider> m_magiciansCombatColliders = new List<Combat.CombatCollider>();
+
+        private void Awake()
+        {
+            Vector3 tempPos = m_plantsContainer.transform.localPosition;
+            tempPos.y = m_startingLocalHeight;
+            m_plantsContainer.transform.localPosition = tempPos;
+            tempPos = m_collider.center;
+            tempPos.y = m_startingLocalHeight;
+            m_collider.center = tempPos;
+            StartCoroutine(ColliderRoutine());
+        }
+
+        private void Update()
+        {
+            if(Time.time - m_timeOfLastHealWave > m_healCooldown)
+            {
+                for(int i = 0; i < m_magiciansCombatColliders.Count; ++i)
+                {
+                    m_magiciansCombatColliders[i].Owner.Heal(m_amountToHeal);
+                }
+                m_timeOfLastHealWave = Time.time;
+            }
+        }
+
+
+
+        private IEnumerator ColliderRoutine()
+        {
+            float timeOfLastIteration = Time.time;
+            yield return null;
+            while (m_plantsContainer.transform.localPosition.y < m_finishingLocalHeight)
+            {
+                Vector3 movementToAdd = Vector3.up * (1 / m_comingOutOfGroundDuration) * (Time.time - timeOfLastIteration);
+                m_plantsContainer.transform.localPosition += movementToAdd;
+                m_collider.center += movementToAdd;
+                timeOfLastIteration = Time.time;
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(m_lifeSpan);
+            timeOfLastIteration = Time.time;
+            yield return null;
+            while (m_plantsContainer.transform.localPosition.y > m_startingLocalHeight)
+            {
+                Vector3 movementToAdd = Vector3.down * (1 / m_goingIntoGroundDuration) * (Time.time - timeOfLastIteration);
+                m_plantsContainer.transform.localPosition += movementToAdd;
+                m_collider.center += movementToAdd;
+                timeOfLastIteration = Time.time;
+                yield return null;
+            }
+            Destroy(gameObject);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.TryGetComponent<MagicianController>(out MagicianController magician)
+                && magician.TryGetComponent<Combat.CombatCollider>(out Combat.CombatCollider collider)
+                && !m_magiciansCombatColliders.Contains(collider))
+            {
+                m_magiciansCombatColliders.Add(collider);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent<MagicianController>(out MagicianController magician)
+                && magician.TryGetComponent<Combat.CombatCollider>(out Combat.CombatCollider collider)
+                && m_magiciansCombatColliders.Contains(collider))
+            {
+                m_magiciansCombatColliders.Remove(collider);
+            }
+        }
+
+
+    }
+}
