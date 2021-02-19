0d0b822efc9f1cbaffa541eb6edc4db7a6ecb0e7
Gretsok <47755843+Gretsok@users.noreply.github.com>
2021-02-19 23:33:30 +0100

    MainMenu

---
+++ ElementaryMagiciansUnityProject/Assets/05_Scripts/MainMenu/MainMenuStateMachine.cs
@@ -0,0 +1,37 @@
+﻿using MOtter.StatesMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ElementaryMagicians.MainMenu
+{
+    public class MainMenuStateMachine : MainStatesMachine
+    {
+        [SerializeField]
+        private ProjElf.SceneData.SceneData m_dunjeonSceneData = null;
+
+        [SerializeField]
+        private State m_mainMenuState = null;
+        [SerializeField]
+        private State m_creditstate = null;
+        public void ClickPlay()
+        {
+            m_dunjeonSceneData.LoadLevel();
+        }
+
+        public void ClickQuit()
+        {
+            Application.Quit();
+        }
+
+        public void ClickCredits()
+        {
+            SwitchToState(m_creditstate);
+        }
+
+        public void ClickMainMenu()
+        {
+            SwitchToState(m_mainMenuState);
+        }
+    }
+}
